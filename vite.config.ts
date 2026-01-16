0a96021469f463e47163e810a8614f3b7f4614dc
nh397912-dotcom <nh397912@gmail.com>
2026-01-16 20:32:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/gom_my_thien/',
+        base: './',
       server: {
         port: 3000,
         host: '0.0.0.0',
