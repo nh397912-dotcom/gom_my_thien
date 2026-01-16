34b0be3c090f6b931eea626776e20b354dd0f147
nh397912-dotcom <nh397912@gmail.com>
2026-01-16 21:40:39 +0700

    feat: Remove Hero component and simplify layout
    The Hero component has been removed and its functionality integrated into the main content flow. The overall layout of the App component has been simplified, and styling adjustments have been made to the Introduction component and global CSS. The `base` path in vite.config.ts has also been removed.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import Header from './components/Header';
-import Hero from './components/Hero';
 import Introduction from './components/Introduction';
 import PotteryProcess from './components/PotteryProcess';
 import ProductGallery from './components/ProductGallery';
@@ -15,14 +14,11 @@ function App() {
     <AuthProvider>
       <div className="min-h-screen flex flex-col">
         <Header />
-        <main className="flex-grow">
-          <Hero />
-          <div className="container mx-auto px-4 py-16 space-y-24">
-            <Introduction />
-            <PotteryProcess />
-            <ProductGallery />
-            <CreativeStudio />
-          </div>
+        <main className="flex-grow container mx-auto px-4 py-8 md:py-12 space-y-16">
+          <Introduction />
+          <PotteryProcess />
+          <ProductGallery />
+          <CreativeStudio />
         </main>
         <Chatbot />
         <Footer />
